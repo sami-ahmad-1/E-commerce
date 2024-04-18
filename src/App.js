453192703d18b24516f69fb1853071cf1a2dd215
Sami Ahmad <samiahmad.0051@gmail.com>
2024-04-18 10:12:46 +0530

    Added upto Cart

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import home from './pages/Home'
 import Login from './features/auth/components/login';
 import SignUp from './features/auth/components/signup';
+import CartPage from './pages/CartPage';
 function App() {
   return (
     <>
-      <Login/>
+      <CartPage/>
     </>
   )
 }
---
+++ src/pages/CartPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cart from '../features/cart/cart'
+function CartPage() {
+  return (
+    <div>
+        <Cart/>
+    </div>
+  )
+}
+
+export default CartPage
