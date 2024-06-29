497af2693cb2eb2a302895d26baed903b0e7c4ac
Sami Ahmad <samiahmad.0051@gmail.com>
2024-06-29 13:04:42 +0530

    .

--- src/features/product/Component/productDetail.js
+++ src/features/product/Component/productDetail.js
@@ -42,7 +42,7 @@ export default function ProductDetail() {
           <div>
             <div className="pt-6">
               <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8" >
-                { prod.images && prod.images.map((val, index) =>
+                {prod.images && prod.images.map((val, index) =>
                   <div key={index} className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
                     <img
                       src={val}
@@ -69,21 +69,16 @@ export default function ProductDetail() {
                         {[0, 1, 2, 3, 4].map((rating) => (
                           <StarIcon
                             key={rating}
-                            className={classNames(                            
+                            className={classNames(
                               `${prod.rating}` > rating ? 'text-yellow-500' : 'text-gray-200',
                               'h-5 w-5 flex-shrink-0'
                             )}
                             aria-hidden="true"
                           />
-                        ))}
+))}
                       </div>
-                      {/* <p className="sr-only">{reviews.average} out of 5 stars</p>
-                          <a href={reviews.href} className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                            {reviews.totalCount} reviews
-                          </a> */}
                     </div>
                   </div>
-
                   <form className="mt-10">
                     {/* Colors */}
                     <div>
